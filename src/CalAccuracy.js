2bfe46b6958c9228ccdbf1ed6155d67b6c2be2bc
Shashank384 <shashank.mayur@gmail.com>
2021-05-31 10:21:34 +0530

    Calculate Accuracy added

--- src/CalAccuracy.js
+++ src/CalAccuracy.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
 function CalAccuracy({correctCharCount, time}) {
-    console.log(correctCharCount, time)
-    return null
+    time = 60-time;
+    if (correctCharCount !== 0 && time !== 0) {
+        const grossWPM = Math.round((correctCharCount/5) / (time/60));
+        return (
+            <div style={{fontSize: "30px", textAlign: "center"}}>{grossWPM} WPM</div>
+        )
+    }    
+    return null;
 }
 
 export default CalAccuracy
